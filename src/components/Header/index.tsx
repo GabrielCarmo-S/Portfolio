920438c7f2770fcc9f0aa5471903d1ad42f84b8a
GabrielCarmo-S <perien.op@gmail.com>
2022-01-10 21:12:33 -0300

    Atualização das config

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div>
+      <h2>EEEE</h2>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
+import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Olá</h1>
+
+      <Header />
     </div>
   )
 }
