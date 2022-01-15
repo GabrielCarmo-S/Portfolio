64d955a40212c7293b1b1f592147e49a2837888c
GabrielCarmo-S <perien.op@gmail.com>
2022-01-15 17:09:00 -0300

    Update

---
+++ src/components/Skills/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss';
+import Image from 'next/image'
+
+
+export function Skills() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+
+      </div>
+    </div>
+  );
+}
