df02eee4d2a779d0efd14d642173e82832d54073
GabrielCarmo-S <perien.op@gmail.com>
2022-01-15 17:21:29 -0300

    Updates

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -7,7 +7,7 @@ export function About() {
       <div className={styles.wrapper}>
         <h2>Sobre mim</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+          Hi, Gabriel Carmo, enthusiast of the best web & mobile development technologies.
         </p>
 
         <div className={styles.icons}>
